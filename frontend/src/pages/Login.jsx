6db67d0e9b383cc319ccaec63295e4f7954c714e
Ananta <anantapandey0902@gmail.com>
2026-01-19 22:36:22 +0530

    changes

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,42 +5,52 @@ import toast, { Toaster } from "react-hot-toast";
 import "./Auth.css";   
 
 const Login = async (req, res) => {
+  try {
 
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-  if (!user) {
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      return res.json({
+        success: false,
+        message: "Wrong password"
+      });
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none"
+    });
+
     return res.json({
+      success: true,
+      message: "login success"
+    });
+
+  } catch (err) {
+    console.log("LOGIN ERROR:", err);
+    res.status(500).json({
       success: false,
-      message: "User not found"
+      message: "server error"
     });
   }
-
-  const valid = await bcrypt.compare(password, user.password);
-
-  if (!valid) {
-    return res.json({
-      success: false,
-      message: "Wrong password"
-    });
-  }
-  const token = jwt.sign(
-    { id: user._id, email: user.email },
-    process.env.JWT_SECRET,
-    { expiresIn: "7d" }
-  );
-
-  res.cookie("token", token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none"
-  });
-
-  return res.json({
-    success: true,
-    message: "login success"
-  });
 };
 
 export default Login;
